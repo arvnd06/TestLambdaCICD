936ee9c028324281ea2f2483e8a6e508dcefe713
Arvind <arvind06eee@example.com>
2025-07-12 22:22:08 +0530

    final commit

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
         Principal = {
           Service = "lambda.amazonaws.com"
         }
@@ -25,11 +25,11 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  function_name = "TestFunction"
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.12"
-  role          = aws_iam_role.lambda_exec_role.arn
-  filename      = "${path.module}/../code.zip"
-  source_code_hash = filebase64sha256("${path.module}/../code.zip")
-  timeout       = 10
+  function_name    = "TestFunction"
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.12"
+  role             = aws_iam_role.lambda_exec_role.arn
+  filename         = "${path.module}/code.zip"
+  source_code_hash = filebase64sha256("${path.module}/code.zip")
+  timeout          = 10
 }
