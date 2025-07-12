f82d56a37a3f13ba358d288899114dfd31deb6ab
Arvind <arvind06eee@example.com>
2025-07-12 23:00:04 +0530

    final commit

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# IAM Role for Lambda Execution
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
 
@@ -19,11 +20,25 @@ resource "aws_iam_role" "lambda_exec_role" {
   })
 }
 
+# Attach Basic Execution Policy
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+# Attach Full Lambda Access Policy
+resource "aws_iam_role_policy_attachment" "lambda_full_access" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambda_FullAccess"
+}
+
+# Attach Full IAM Access Policy (for dev/testing only)
+resource "aws_iam_role_policy_attachment" "iam_full_access" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+
+# Lambda Function Resource
 resource "aws_lambda_function" "test_lambda" {
   function_name    = "TestFunction"
   handler          = "lambda_function.lambda_handler"
